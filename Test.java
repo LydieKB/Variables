6407eba6d23725ff6d6678f0d83436914ee97e64
LydieKB <68040063+LydieKB@users.noreply.github.com>
2020-07-22 09:19:27 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Variables{
+int age = 29 ;
+static char my_initial = 'L';
+static String first_name = "Lydie ";
+    public static void main(String[] args) {
+int year = 2020 ;
+        System.out.println( " We are in " + year + " My name is " + first_name + "My initial is " + my_initial ); 
+        Variables obj = new Variables();
+        System.out.println("I have " + obj.age +" years old ");
+
+    }
+}
